26de2a9ff0995e9e183cc3b00c34b9cb1e415553
Andrew Phillips <andrew-phillips98@hotmail.com>
2024-06-21 11:18:37 +1000

    exercises up to 4.12

---
+++ part4/notes-backend/mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://mongo:${password}@cluster0.gyfjqlb.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'Here is another note',
+  important: true,
+})
+
+
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
