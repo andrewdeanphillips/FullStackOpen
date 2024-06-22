97c528504941aec5f6e0b0604b7574fa2dbd9068
Andrew Phillips <andrewdeanphillips@outlook.com>
2024-06-23 09:17:47 +1000

    exercise 4.16 completed

--- part4/bloglist/tests/user_api.test.js
+++ part4/bloglist/tests/user_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -43,6 +43,50 @@ test('users can be viewed', async () => {
         .expect('Content-Type', /application\/json/)
     const usernames = users.body.map(u => u.username)
     assert(usernames.includes('bbam'))
+})
+
+describe('User details are validated before being added to db', () => {
+    test('username under 3 chars cannot be added', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const response = await api
+            .post('/api/users')
+            .send(helper.shortUsernameUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+            assert(response.body.error.includes('username')
+            && response.body.error.includes('is shorter than the minimum allowed length (3)'));
+
+        const usersAtEnd = await helper.usersInDb()
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    })
+
+    test('password under 3 chars cannot be added', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        await api
+            .post('/api/users')
+            .send(helper.shortPasswordUser)
+            .expect(400)
+
+        const usersAtEnd = await helper.usersInDb()
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    })
+
+    test('usernames must be unique', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const response = await api
+            .post('/api/users')
+            .send(helper.existingUsernameUser)
+            .expect(400)
+
+        assert(response.body.error.includes('expected `username` to be unique'))
+
+        const usersAtEnd = await helper.usersInDb()
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    })
 })
 
 
