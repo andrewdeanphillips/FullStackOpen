c759b9618688e9d642e9a56bf63a9463b9692ede
Andrew Phillips <andrew-phillips98@hotmail.com>
2024-07-15 18:02:18 +1000

    exercise 6.13 completed

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,12 +1,14 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
+import Notification from './components/Notification'
 import { addVoteTo, addAnecdote } from './reducers/anecdoteReducer'
 import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
   return (
     <div>
+      <Notification />
       <h2>Anecdotes</h2>
       <Filter />
       <AnecdoteList />
